df452f63ea3ad74054f76ebfcfb7627caeb87ef8
Jo <loveit6507@gmail.com>
2021-07-20 17:19:04 +0900

    Feat: 생성 서블릿 페이지
    별도의 디자인은 설정하지 않음
    공식 API 활용하여 실행했을 때 현재시각이 화면에 나오도록 구성

---
+++ aboutme/src/main/java/TodayServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodayServlet
+ */
+@WebServlet("/today")
+public class TodayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodayServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+		String formatDate = LocalDateTime.now().format(formatter);
+		PrintWriter out = response.getWriter();
+		out.println("<a href='../aboutme/index.html'>메인화면</a>");
+		out.print("<h1>현재시간 : "+formatDate+"</h1>");
+		
+	}
+
+}
