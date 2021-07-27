c2ff7003eadffe5aaa73c38e82ce40bdbd307174
Jo <loveit6507@gmail.com>
2021-07-27 14:49:56 +0900

    Refactor: 정리 소스코드
    불필요 코드 삭제
    URI 경로 수정
    오버라이드 어노테이션 삽입

--- aboutme/src/main/java/TodayServlet.java
+++ aboutme/src/main/java/TodayServlet.java
@@ -17,25 +17,18 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TodayServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html;charset=utf-8");
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
 		String formatDate = LocalDateTime.now().format(formatter);
 		PrintWriter out = response.getWriter();
-		out.println("<a href='../aboutme/index.html'>메인화면</a>");
+		out.println("<a href='/aboutme/index.html'>메인화면</a>");
 		out.print("<h1>현재시간 : "+formatDate+"</h1>");
 		
 	}
